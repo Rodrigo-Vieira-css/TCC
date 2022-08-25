d3c0cff98b9b358f7ee313cc463b738a9ba7fd6b
Rodrigo Vieira <83049956+Rodrigo-Vieira-css@users.noreply.github.com>
2022-08-25 09:36:14 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,36 +1,44 @@
-const barra = document.querySelector('.barra');
-const bars = document.querySelector('#close');
-const title = document.querySelector('.title');
-const sidebar = document.querySelector('.sidebar');
-const search = document.querySelector('.search');
+function movSidebar() {
+    const barra = document.querySelector('.barra');
+    const bars = document.querySelector('#close');
+    const title = document.querySelector('.title');
+    const sidebar = document.querySelector('.sidebar');
+    const search = document.querySelector('.search');
 
-barra.addEventListener('click', (e) => {
-    const el = e.target
-    e.preventDefault();
-    fechar();
-});
-
-function fechar() {
-    sidebar.style.display = "none";
-    title.setAttribute('class', 'title-ajust')
-    bars.setAttribute('class', 'las la-bars');
-    ajustar(true)
-}
-function abrir() {
-    sidebar.style.display = "block";
-    bars.setAttribute('class', 'las la-times');
-}
-
-function ajustar(n) {
-    if (n === true) {
-    bars.style.marginLeft = "-20rem";
-    title.style.marginLeft = "-17rem";
-    title.style.paddingRight ="10rem";  
-    search.style.paddingRight = "13rem";
-    bars.addEventListener('click', (e)=> {
-       abrir();
-    })
-    } else if (n === false) {
-
+    barra.addEventListener('click', (e) => {
+        const el = e.target
+        e.preventDefault();
+        fechar();
+    });
+    function fechar() {
+        ajustarFechar();
+        bars.setAttribute('class', 'las la-bars');
+        barra.addEventListener('click', () => {
+            abrir()
+        })
     }
+    function abrir() {
+        ajustarAbrir();
+        bars.setAttribute('class', 'las la-times-circle');
+        barra.addEventListener('click', () => {
+            fechar();
+
+        })
+    }
+    function ajustarAbrir() {
+        sidebar.style.display = "block";
+        bars.style.marginLeft = "1rem";
+        title.style.marginLeft = ".5rem";
+        title.style.paddingRight = "1rem";
+        search.style.paddingRight = "";
+    }
+    function ajustarFechar() {
+        sidebar.style.display = "none";
+        bars.style.marginLeft = "-20rem";
+        title.style.marginLeft = "-17rem";
+        title.style.paddingRight = "10rem";
+        search.style.paddingRight = "13rem";
+    }
+
 }
+movSidebar();
